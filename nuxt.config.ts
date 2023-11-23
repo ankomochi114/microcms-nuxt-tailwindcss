3b2a18bf4bc2275ce4aee253942f7eb0f3bda8d8
ankomochi114 <s.kotomi.0114@gmail.com>
2023-11-24 00:17:28 +0900

    修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,6 @@ export default defineNuxtConfig({
     baseURL: SITE_URL,
     cdnURL: `https://isystk.github.io${SITE_URL}`,
   },
-  nitro: {
-    prerender: {
-      failOnError: false,
-    },
-  },
   tailwindcss: {
     config: {
       content: [],
